e06ad77c0be67241f3da04e230e71fe9e2f618e1
Naoki Kishimoto <kishimoto.naoki@lmi.ne.jp>
2019-10-07 12:02:19 +0000

    ユーザー登録まで完了

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,18 @@ module BookApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    #----------ここから下を追加---------------------------
+    #RSpecを利用したコントローラの機能テストは、Rails5からはrequest specで
+    #記述することが推奨されるみたいなので、controller_specsも省きました。
+    config.generators do |g|
+      g.test_framework :rspec,
+                       helper_specs: false,
+                       routing_specs: false,
+                       view_specs: false,
+                       controller_specs: false
+    end
+    #----------ここまで-----------------------------------
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
